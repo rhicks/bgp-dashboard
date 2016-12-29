d07429a4d5de19337c9b77159c4120dd5b5ab8c9
Richard Hicks <rhicks@nero.net>
2016-12-29 13:10:48 -0800

    remove comments, jsonify the output

--- bgp-query.py
+++ bgp-query.py
@@ -12,42 +12,29 @@ def take(n, iterable):
     "Return first n items of the iterable as a list"
     return list(islice(iterable, n))
 
+def db_connect():
+    client = MongoClient()
+    return(client.bgp)
+
 def asn_name_query(asn):
     if asn == None:
         asn = 3701
     if 64512 <= asn <= 65534:
         return("RFC6996 - Private Use ASN")
     else:
-        # query = 'as' + str(asn) + '.asn.cymru.com'
-        # resolver = dns.resolver.Resolver()
-        # resolver.timeout = 10
-        # resolver.lifetime = 1
         try:
             query = 'as' + str(asn) + '.asn.cymru.com'
-            #print(query)
             resolver = dns.resolver.Resolver()
-            #print(resolver)
             answers = resolver.query(query, 'TXT')
-            #print(answers)
             for rdata in answers:
-                #print(rdata)
                 return(str(rdata).split("|")[-1].split(",",2)[0].strip())
-                # for txt_string in rdata.strings:
-                #     print(txt_string)
-                #     return(txt_string.split("|")[-1].split(",", 2)[0].strip())
         except:
             return("(DNS Error)")
 
 def reverse_dns_query(ip):
-    # resolver.timeout = 1
-    # resolver.lifetime = 1
     try:
         addr = dns.reversename.from_address(str(ip))
-        #return(addr)
         resolver = dns.resolver.Resolver()
-        # resolver.timeout = 1
-        # resolver.lifetime = 1
-        # #print(str(resolver.query(addr,"PTR")[0])[:-1])
         return str(resolver.query(addr,"PTR")[0])[:-1]
     except:
         return("(DNS Error)")
@@ -64,8 +51,7 @@ def find_network(ip, netmask):
         return(find_network(ip, netmask-1))
 
 def avg_as_path_length():
-    client = MongoClient()
-    db = client.bgp
+    db = db_connect()
     as_path_counter = 0
 
     all = db.bgp.find()
@@ -74,114 +60,79 @@ def avg_as_path_length():
             as_path_counter += len(set(prefix['as_path']))
         except:
             pass
-    print(round(as_path_counter/all.count(), 3))
+    path_length = round(as_path_counter/all.count(), 3)
+    return json.dumps({'avg_as_path_length': path_length})
 
 
 def top_peers(count):
-    client = MongoClient()
-    db = client.bgp
+    db = db_connect()
     top_peers_dict = {}
     peers = db.bgp.distinct("next_hop_asn")
+    json_data = []
 
     for peer in peers:
         prefixes = db.bgp.find({"next_hop_asn": peer})
         top_peers_dict[peer] = prefixes.count()
     top_n = take(count, sorted(top_peers_dict.items(), key=lambda x: x[1], reverse=True))
     for asn in top_n:
-        print("%d - %d - %s" % (asn[1], asn[0], asn_name_query(asn[0])))
+        json_data.append({
+            'asn': asn[0],
+            'count': asn[1],
+            'name': asn_name_query(asn[0])})
+    return(json.dumps(json_data, indent=2))
 
-def ipv4_cidr_breakdown():
-    client = MongoClient()
-    db = client.bgp
+
+def cidr_breakdown():
+    db = db_connect()
     all_prefixes = db.bgp.find()
     ipv4_list = []
     ipv6_list = []
-
+    json_data = []
+    bads_list = []
 
     for prefix in all_prefixes:
         if prefix['ip_version'] == 4:
             ipv4_list.append(int(prefix['prefix'].split('/',1)[1]))
+            if int(prefix['prefix'].split('/',1)[1]) > 24:
+                bads_list.append({
+                'origin_as': int(prefix['origin_as']),
+                'prefix': prefix['prefix']})
         if prefix['ip_version'] == 6:
             ipv6_list.append(int(prefix['prefix'].split('/',1)[1]))
 
     ipv4_count = list(Counter(ipv4_list).items())
-    print("### IPv4 CIDR Count ###")
-    for mask, count in ipv4_count:
-        print("Mask: /%d - Count: %d" % (mask, count))
-    print()
     ipv6_count = list(Counter(ipv6_list).items())
-    print("### IPv6 CIDR Count ###")
+
+    for mask, count in ipv4_count:
+        json_data.append({
+            'mask': mask,
+            'count': count,
+            'ip_version': 4})
     for mask, count in ipv6_count:
-        print("Mask: /%d - Count: %d" % (mask, count))
+        json_data.append({
+            'mask': mask,
+            'count': count,
+            'ip_version': 6})
+
+    cidr_list = json.dumps(json_data, indent=2)
+    bad_list  = json.dumps(bads_list, indent=2)
+
+    return(cidr_list, bad_list)
+
+def communities_count():
+    db = db_connect()
+    communities = db.bgp.distinct("communities")
+    json_data = []
+
+    for comm in communities:
+        json_data.append({
+            'community': comm,
+            'count': db.bgp.find({'communities': {'$regex' : comm}}).count()})
+
+    return(json.dumps(json_data, indent=2))
 
 
-
-avg_as_path_length()
-top_peers(10)
-ipv4_cidr_breakdown()
-
-
-# client = MongoClient()
-# db = client.bgp
-#
-# # cursor = db.restaurants.find({"borough": "Manhattan"})
-# #
-# # for document in cursor:
-# #     print(document)
-#
-# peer_asns = db.bgp.distinct("next_hop_asn")
-# next_hop_ips = db.bgp.distinct("nexthop")
-# origin_asns =  db.bgp.distinct("origin_as")
-# google = db.bgp.find({"next_hop_asn": 15169})
-# isp = db.bgp.find({"origin_as": 15169})
-# communities = db.bgp.distinct("communities")
-# cidr = db.bgp.find()
-#
-# ip = '157.246.0.0'
-# subnet_mask = 24
-#
-# print("Total Prefixes: ", db.bgp.count())
-# print("Total Peers: ", len(peer_asns))
-# print("Next Hop IP Address: ", len(next_hop_ips))
-# print("Origin ASNs: ", len(origin_asns))
-#
-# #print("Avg AS Path Length:")
-# #print("ISP:", isp)
-# #
-# # for comm in communities:
-# #     print('%s - %s' % (comm, db.bgp.find({'communities': {'$regex' : comm}}).count()))
-# #
-# # cidr_list = []
-# # bads_list = []
-# # for prefix in cidr:
-# #     if prefix['ip_version'] == 4:
-# #         cidr_list.append(int(prefix['prefix'].split('/',1)[1]))
-# #         if int(prefix['prefix'].split('/',1)[1]) > 24:
-# #             bads_list.append('%d : %s' % (int(prefix['origin_as']), prefix['prefix']))
-# #             # print('%s : %s' % (prefix['origin_as'], prefix['prefix']))
-# #         # print(Counter(cidr_list))
-# #
-# # bads_list.sort()
-# # for blah in bads_list:
-# #     print(blah)
-# # print(bads_list)
-# # # for mask in cidr_list:
-# # #     print('%s' : '%d' % (mask, ))
-# # # print(Counter(cidr_list).keys())
-# # # print(Counter(cidr_list).values())
-# # #print(find_network(ip, subnet_mask))
-# # # print("Google: ")
-# # #for prefix in isp:
-# #     #print(prefix)
-# # #    print(asn_name_query(prefix['origin_as']))
-# #
-# # # for ip in next_hop_ips:
-# # #     print(reverse_dns_query(ip))
-# # # for peer in peer_asns:
-# # #     print(str(peer) + ' - ' + asn_name_query(peer) + ": ", db.bgp.count({"next_hop_asn": peer}))
-# #
-# #
-# # # Create Functions for the following:
-# # # - Avg as_path (should give a good representation about overall connectivity)
-# # # - IPv4 Netmask distribution and count per
-# # # - IPv6 Netmask distribution and count per
+print(avg_as_path_length())
+print(top_peers(10))
+print(cidr_breakdown()[0])
+print(communities_count())
