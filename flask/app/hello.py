fdd044f5f17bef72ec6c7dd7cf518898200dd4b4
Richard Hicks <rhicks@nero.net>
2017-01-21 16:27:57 -0800

    refactor for pep8

--- flask/app/hello.py
+++ flask/app/hello.py
@@ -1,7 +1,4 @@
-from flask import Flask, jsonify, url_for, request, render_template
-import json
-import requests
-import pymongo
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
 import dns.resolver
 import ipaddress
@@ -13,21 +10,25 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+
 def db_connect():
     client = MongoClient(host='mongo')
     return(client.bgp)
 
+
 def take(n, iterable):
-    "Return first n items of the iterable as a list"
+    'Return first n items of the iterable as a list'
     return list(islice(iterable, n))
+
 
 def find_network(ip, netmask):
     try:
         if ipaddress.ip_address(ip).version == 4:
             db = db_connect()
-            network = str(ipaddress.ip_network(ipaddress.ip_address(ip)).supernet(new_prefix=netmask))
-            result = db.bgp.find_one({"prefix": network})
-            if result != None:
+            network = str(ipaddress.ip_network(
+                ipaddress.ip_address(ip)).supernet(new_prefix=netmask))
+            result = db.bgp.find_one({'prefix': network})
+            if result is not None:
                 return(result)
             elif netmask == 0:
                 return(None)
@@ -35,9 +36,10 @@ def find_network(ip, netmask):
                 return(find_network(ip, netmask-1))
         elif ipaddress.ip_address(ip).version == 6:
             db = db_connect()
-            network = str(ipaddress.ip_network(ipaddress.ip_address(ip)).supernet(new_prefix=netmask + 32))
-            result = db.bgp.find_one({"prefix": network})
-            if result != None:
+            network = str(ipaddress.ip_network(
+                ipaddress.ip_address(ip)).supernet(new_prefix=netmask + 32))
+            result = db.bgp.find_one({'prefix': network})
+            if result is not None:
                 return(result)
             elif netmask == 0:
                 return(None)
@@ -48,54 +50,62 @@ def find_network(ip, netmask):
     except:
         return(None)
 
+
 def asn_name_query(asn):
-    if asn == None:
+    if asn is None:
         asn = 3701
     if 64512 <= asn <= 65534:
-        return("RFC6996 - Private Use ASN")
+        return('RFC6996 - Private Use ASN')
     else:
         try:
             query = 'as' + str(asn) + '.asn.cymru.com'
             resolver = dns.resolver.Resolver()
             answers = resolver.query(query, 'TXT')
             for rdata in answers:
-                return(str(rdata).split("|")[-1].split(",",2)[0].strip())
+                return(str(rdata).split('|')[-1].split(',', 2)[0].strip())
         except:
-            return("(DNS Error)")
+            return('(DNS Error)')
+
 
 def is_peer(asn):
     db = db_connect()
-    peers = db.bgp.distinct("next_hop_asn")
+    peers = db.bgp.distinct('next_hop_asn')
     if asn in peers:
         return True
     else:
         return False
 
+
 def reverse_dns_query(ip):
     try:
         addr = dns.reversename.from_address(str(ip))
         resolver = dns.resolver.Resolver()
-        return str(resolver.query(addr,"PTR")[0])[:-1]
+        return str(resolver.query(addr, 'PTR')[0])[:-1]
     except:
-        return("(DNS Error)")
+        return('(DNS Error)')
+
 
 def peer_count():
     db = db_connect()
-    peer_asns = db.bgp.distinct("next_hop_asn")
+    peer_asns = db.bgp.distinct('next_hop_asn')
     return(len(peer_asns))
+
 
 def prefix_count(version):
     db = db_connect()
-    result = db.bgp.find({"ip_version": version})
+    result = db.bgp.find({'ip_version': version})
     return(result.count())
+
 
 def nexthop_ip_count():
     db = db_connect()
-    nexthop_ip_count = db.bgp.distinct("nexthop")
+    nexthop_ip_count = db.bgp.distinct('nexthop')
     return(len(nexthop_ip_count))
 
+
 def epoch_to_date(epoch):
-    return(time.strftime("%Y-%m-%d %H:%M:%S %Z", time.gmtime(epoch)))
+    return(time.strftime('%Y-%m-%d %H:%M:%S %Z', time.gmtime(epoch)))
+
 
 def avg_as_path_length():
     db = db_connect()
@@ -110,22 +120,26 @@ def avg_as_path_length():
     path_length = round(as_path_counter/(all.count() * 1.0), 3)
     return(path_length)
 
+
 def top_peers(count):
     db = db_connect()
     top_peers_dict = {}
-    peers = db.bgp.distinct("next_hop_asn")
+    peers = db.bgp.distinct('next_hop_asn')
     json_data = []
 
     for peer in peers:
-        prefixes = db.bgp.find({"next_hop_asn": peer})
+        prefixes = db.bgp.find({'next_hop_asn': peer})
         top_peers_dict[peer] = prefixes.count()
-    top_n = take(count, sorted(top_peers_dict.items(), key=lambda x: x[1], reverse=True))
+    top_n = take(count, sorted(top_peers_dict.items(),
+                               key=lambda x: x[1],
+                               reverse=True))
     for asn in top_n:
         json_data.append({
             'asn': asn[0],
             'count': asn[1],
             'name': asn_name_query(asn[0])})
     return(json_data)
+
 
 def peers():
     db = db_connect()
@@ -137,9 +151,9 @@ def peers():
     for prefix in peers_list:
         myset.add(prefix['next_hop_asn'])
     for asn in myset:
-        ipv4_count  = db.bgp.find({"next_hop_asn": asn,"ip_version": 4}).count()
-        ipv6_count = db.bgp.find({"next_hop_asn": asn,"ip_version": 6}).count()
-        if asn == None:
+        ipv4_count = db.bgp.find({'next_hop_asn': asn, 'ip_version': 4}).count()
+        ipv6_count = db.bgp.find({'next_hop_asn': asn, 'ip_version': 6}).count()
+        if asn is None:
             asn = 3701
         json_data.append({
             'asn': asn,
@@ -147,6 +161,7 @@ def peers():
             'ipv4_count': ipv4_count,
             'ipv6_count': ipv6_count})
     return(json_data)
+
 
 def customers():
     db = db_connect()
@@ -156,15 +171,14 @@ def customers():
     ipv6_prefix_counter = 0
 
     customers_list = db.bgp.find({'communities': '3701:370'})
-    #peers_list = db.bgp.find()
     for prefix in customers_list:
         myset.add(prefix['next_hop_asn'])
     for asn in myset:
-        ipv4_count  = db.bgp.find({"next_hop_asn": asn,"ip_version": 4}).count()
-        ipv6_count = db.bgp.find({"next_hop_asn": asn,"ip_version": 6}).count()
+        ipv4_count = db.bgp.find({'next_hop_asn': asn, 'ip_version': 4}).count()
+        ipv6_count = db.bgp.find({'next_hop_asn': asn, 'ip_version': 6}).count()
         ipv4_prefix_counter += ipv4_count
         ipv6_prefix_counter += ipv6_count
-        if asn == None:
+        if asn is None:
             asn = 3701
         json_data.append({
             'asn': asn,
@@ -172,6 +186,7 @@ def customers():
             'ipv4_count': ipv4_count,
             'ipv6_count': ipv6_count})
     return(json_data, ipv4_prefix_counter, ipv6_prefix_counter)
+
 
 def cidr_breakdown():
     db = db_connect()
@@ -183,13 +198,12 @@ def cidr_breakdown():
 
     for prefix in all_prefixes:
         if prefix['ip_version'] == 4:
-            ipv4_list.append(int(prefix['prefix'].split('/',1)[1]))
-            if int(prefix['prefix'].split('/',1)[1]) > 24:
-                bads_list.append({
-                'origin_as': int(prefix['origin_as']),
-                'prefix': prefix['prefix']})
+            ipv4_list.append(int(prefix['prefix'].split('/', 1)[1]))
+            if int(prefix['prefix'].split('/', 1)[1]) > 24:
+                bads_list.append({'origin_as': int(prefix['origin_as']),
+                                  'prefix': prefix['prefix']})
         if prefix['ip_version'] == 6:
-            ipv6_list.append(int(prefix['prefix'].split('/',1)[1]))
+            ipv6_list.append(int(prefix['prefix'].split('/', 1)[1]))
 
     ipv4_count = list(Counter(ipv4_list).items())
     ipv6_count = list(Counter(ipv6_list).items())
@@ -207,22 +221,25 @@ def cidr_breakdown():
 
     return(json_data)
 
+
 def communities_count():
     db = db_connect()
-    communities = db.bgp.distinct("communities")
+    communities = db.bgp.distinct('communities')
     json_data = []
 
     for comm in communities:
         json_data.append({
             'community': comm,
-            'count': db.bgp.find({'communities': {'$regex' : comm}}).count()})
+            'count': db.bgp.find({'communities': {'$regex': comm}}).count()})
 
     return(json_data)
+
 
 @app.route('/', methods=['GET'])
 def index():
     # num_peers = peer_count()
     return render_template('home.html', **locals())
+
 
 @app.route('/hello/', methods=['GET'])
 def hello_index():
@@ -233,11 +250,11 @@ def hello_index():
     peers = data['peers']
     return render_template('hello.html', **locals())
 
+
 @app.route('/search/<query>', methods=['GET'])
 def search_index(query):
     db = db_connect()
     number = 0
-    string = None
     prefixes = []
     for t in query.split():
         try:
@@ -249,19 +266,20 @@ def search_index(query):
     except:
         pass
     network = find_network(query, netmask=32)
-    if network == None:
-        result = db.bgp.find({ "$or": [{ 'next_hop_asn': int(number)}, {'prefix': {'$regex': str(query)}}] })
+    if network is None:
+        result = db.bgp.find({'$or': [{'next_hop_asn': int(number)},
+                                      {'prefix': {'$regex': str(query)}}]})
         for network in result:
             prefixes.append({'origin_as': network['origin_as'],
-                            'nexthop': network['nexthop'],
-                            'as_path': network['as_path'],
-                            'prefix': network['prefix'],
-                            'next_hop_asn': network['next_hop_asn'],
-                            'updated': epoch_to_date(network['timestamp']),
-                            'name': asn_name_query(network['origin_as']),
-                            'med': network['med'],
-                            'local_pref': network['local_pref'],
-                            'communities': network['communities']})
+                             'nexthop': network['nexthop'],
+                             'as_path': network['as_path'],
+                             'prefix': network['prefix'],
+                             'next_hop_asn': network['next_hop_asn'],
+                             'updated': epoch_to_date(network['timestamp']),
+                             'name': asn_name_query(network['origin_as']),
+                             'med': network['med'],
+                             'local_pref': network['local_pref'],
+                             'communities': network['communities']})
         return jsonify({'prefixes': prefixes})
     else:
         return jsonify({'origin_as': network['origin_as'],
@@ -275,11 +293,11 @@ def search_index(query):
                         'local_pref': network['local_pref'],
                         'communities': network['communities']})
 
+
 @app.route('/bgp/api/v1.0/ip/<ip>', methods=['GET'])
 def get_ip(ip):
-    db = db_connect()
     network = find_network(ip, netmask=32)
-    if network == None:
+    if network is None:
         return jsonify({})
     else:
         return jsonify({'origin_as': network['origin_as'],
@@ -293,12 +311,13 @@ def get_ip(ip):
                         'local_pref': network['local_pref'],
                         'communities': network['communities']})
 
+
 @app.route('/bgp/api/v1.0/asn/<int:asn>', methods=['GET'])
 def get_asn_prefixes(asn):
     db = db_connect()
     prefixes = []
 
-    routes = db.bgp.find({"origin_as": asn})
+    routes = db.bgp.find({'origin_as': asn})
 
     for prefix in routes:
         prefixes.append({'prefix': prefix['prefix'],
@@ -316,20 +335,20 @@ def get_asn_prefixes(asn):
                     'is_peer': is_peer(asn),
                     'origin_prefix_list': prefixes})
 
+
 @app.route('/bgp/api/v1.0/peers', methods=['GET'])
 def get_peers():
     db = db_connect()
     peers = []
 
-    peer_asns = db.bgp.distinct("next_hop_asn")
+    peer_asns = db.bgp.distinct('next_hop_asn')
 
     for asn in peer_asns:
-        next_hop_ips = db.bgp.find({"next_hop_asn": asn}).distinct("nexthop")
-        if asn == None:
+        next_hop_ips = db.bgp.find({'next_hop_asn': asn}).distinct('nexthop')
+        if asn is None:
             asn = 3701
-        url = request.url_root + 'bgp/api/v1.0/peer/' + str(asn)
-        isp_origin_as = db.bgp.find({"origin_as": asn})
-        isp_nexthop_as = db.bgp.find({"next_hop_asn": asn})
+        isp_origin_as = db.bgp.find({'origin_as': asn})
+        isp_nexthop_as = db.bgp.find({'next_hop_asn': asn})
         if isp_nexthop_as.count() > isp_origin_as.count():
             transit_provider = True
         else:
@@ -343,20 +362,16 @@ def get_peers():
 
     return jsonify({'peers': peers})
 
-@app.route('/bgp/api/v1.0/peers/count', methods=['GET'])
-def get_peer_count():
-    db = db_connect()
-    peers = []
-
-    return jsonify({'peer_count': peer_count()})
 
 @app.route('/bgp/api/v1.0/stats', methods=['GET'])
 def get_stats():
     return myStats.get_json()
 
+
 @app.route('/bgp/api/v1.0/stats/advanced', methods=['GET'])
 def advanced_stats():
     return myStats.get_json()
+
 
 @app.route('/bgp/api/v1.0/asn/<int:asn>/transit', methods=['GET'])
 def get_transit_prefixes(asn):
@@ -378,55 +393,55 @@ def get_transit_prefixes(asn):
                     'transit_prefix_count': len(prefixes),
                     'transit_prefix_list': prefixes})
 
+
 class Stats(object):
     def __init__(self):
-        self.peer_counter       = 0
-        self.ipv4_table_size    = 0
-        self.ipv6_table_size    = 0
+        self.peer_counter = 0
+        self.ipv4_table_size = 0
+        self.ipv6_table_size = 0
         self.nexthop_ip_counter = 0
         self.avg_as_path_length = 0
-        self.top_n_peers        = None
-        self.cidr_breakdown     = None
-        self.communities        = None
-        self.peers              = None
-        self.customers          = None
-        self.customer_count     = 0
-        self.customer_ipv4_prefixes  = 0
-        self.customer_ipv6_prefixes  = 0
-        self.timestamp          = epoch_to_date(time.time())
+        self.top_n_peers = None
+        self.cidr_breakdown = None
+        self.communities = None
+        self.peers = None
+        self.customers = None
+        self.customer_count = 0
+        self.customer_ipv4_prefixes = 0
+        self.customer_ipv6_prefixes = 0
+        self.timestamp = epoch_to_date(time.time())
 
     def get_json(self):
-        return jsonify({'peer_count':         self.peer_counter,
-                        'ipv4_table_size':    self.ipv4_table_size,
-                        'ipv6_table_size':    self.ipv6_table_size,
-                        'nexthop_ip_count':   self.nexthop_ip_counter,
+        return jsonify({'peer_count': self.peer_counter,
+                        'ipv4_table_size': self.ipv4_table_size,
+                        'ipv6_table_size': self.ipv6_table_size,
+                        'nexthop_ip_count': self.nexthop_ip_counter,
                         'avg_as_path_length': self.avg_as_path_length,
-                        'top_n_peers':        self.top_n_peers,
-                        'cidr_breakdown':     self.cidr_breakdown,
-                        'communities':        self.communities,
-                        'peers':              self.peers,
-                        'customers':          self.customers,
-                        'customer_count':     self.customer_count,
-                        'customer_ipv4_prefixes':  self.customer_ipv4_prefixes,
-                        'customer_ipv6_prefixes':  self.customer_ipv6_prefixes,
-                        'timestamp':          self.timestamp})
+                        'top_n_peers': self.top_n_peers,
+                        'cidr_breakdown': self.cidr_breakdown,
+                        'communities': self.communities,
+                        'peers': self.peers,
+                        'customers': self.customers,
+                        'customer_count': self.customer_count,
+                        'customer_ipv4_prefixes': self.customer_ipv4_prefixes,
+                        'customer_ipv6_prefixes': self.customer_ipv6_prefixes,
+                        'timestamp': self.timestamp})
 
     def get_data(self):
-        return        ({'peer_count':         self.peer_counter,
-                        'ipv4_table_size':    self.ipv4_table_size,
-                        'ipv6_table_size':    self.ipv6_table_size,
-                        'nexthop_ip_count':   self.nexthop_ip_counter,
-                        'avg_as_path_length': self.avg_as_path_length,
-                        'top_n_peers':        self.top_n_peers,
-                        'cidr_breakdown':     self.cidr_breakdown,
-                        'communities':        self.communities,
-                        'peers':              self.peers,
-                        'customers':          self.customers,
-                        'customer_count':     self.customer_count,
-                        'customer_ipv4_prefixes':  self.customer_ipv4_prefixes,
-                        'customer_ipv6_prefixes':  self.customer_ipv6_prefixes,
-                        'timestamp':          self.timestamp})
-
+        return ({'peer_count': self.peer_counter,
+                 'ipv6_table_size': self.ipv6_table_size,
+                 'ipv4_table_size': self.ipv4_table_size,
+                 'nexthop_ip_count': self.nexthop_ip_counter,
+                 'avg_as_path_length': self.avg_as_path_length,
+                 'top_n_peers': self.top_n_peers,
+                 'cidr_breakdown': self.cidr_breakdown,
+                 'communities': self.communities,
+                 'peers': self.peers,
+                 'customers': self.customers,
+                 'customer_count': self.customer_count,
+                 'customer_ipv4_prefixes': self.customer_ipv4_prefixes,
+                 'customer_ipv6_prefixes': self.customer_ipv6_prefixes,
+                 'timestamp': self.timestamp})
 
     def update_stats(self):
         self.peer_counter = peer_count()
@@ -446,6 +461,7 @@ class Stats(object):
         self.customers = customers()[0]
         self.communities = communities_count()
         self.timestamp = epoch_to_date(time.time())
+
 
 sched = BackgroundScheduler()
 myStats = Stats()
