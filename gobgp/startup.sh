ca49be0ef71045bb36db43d004192bb6d505708e
Richard Hicks <rhicks@nero.net>
2017-01-25 14:58:18 -0800

    add pip install of requirements and change startup.sh to automatically push bgp data into mongodb

--- gobgp/startup.sh
+++ gobgp/startup.sh
@@ -5,5 +5,8 @@
 # gobgp global rib -a ipv6 -j > /var/tmp/log/ipv6-rib.json
 # echo "dumping ipv4 rib"
 # gobgp global rib -a ipv4 -j > /var/tmp/log/ipv4-rib.json
-# echo "starting monitor"
+cat /dev/null > /var/tmp/log/updates.json
+/usr/bin/tail -f -n +1 /var/tmp/log/updates.json | /var/tmp/bgp-mongo-bulk-load.py &
 gobgp monitor global rib -j > /var/tmp/log/updates.json
+# echo "BGP dump to MongoDB"
+# gobgp monitor global rib -j | /var/tmp/bgp-mongo-bulk-load.py
