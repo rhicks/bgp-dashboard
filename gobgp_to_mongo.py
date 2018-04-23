a4bd4a8c5937024c7d3153be43b381760ddbe134
Richard Hicks <richard.hicks@gmail.com>
2018-04-23 12:03:52 -0700

    update gobgp_to_mongo script to support history and full BGP attributes

--- gobgp_to_mongo.py
+++ gobgp_to_mongo.py
@@ -1,137 +1,157 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
-import json
-from pymongo import MongoClient
-import fileinput
-import ipaddress
 import sys
+import json
+import constants as C
+from pymongo import MongoClient
+from copy import copy
+from datetime import datetime
 
 
-def db_connect():
+def db_connect(host='mongodb'):
     """Return a connection to the Mongo Database."""
-    client = MongoClient(host='mongo')
+    client = MongoClient(host=host)
     return client.bgp
 
 
 def initialize_database(db):
-    """Drop existing data and create indexes"""
-    db.bgp.drop()
-    db.bgp.create_index('prefix')
-    db.bgp.create_index('next_hop_asn')
-    db.bgp.create_index('origin_as')
-    db.bgp.create_index('nexthop')
-    db.bgp.create_index('as_path')
-    db.bgp.create_index('med')
-    db.bgp.create_index('local_pref')
-    db.bgp.create_index('withdrawal')
-    db.bgp.create_index('ip_version')
-    db.bgp.create_index('communities')
-    db.bgp.create_index('prefix')
+    """If the db contains any entries set them all to 'active': False"""
+    db.bgp.update_many(
+        {"active": True},  # Search for
+        {"$set": {"active": False}})  # Replace with
 
 
-def build_json_update_entry(update_entry):
-    """Take individual update entries from GoBGP and build json objects to be
-    consumed as MonogoDB entries."""
-    nexthop = as_path = next_hop_asn = origin_as = med = local_pref = withdrawal = None
-    communities = []
-    prefix = update_entry['nlri']['prefix']
-    ip_version = ipaddress.ip_address(prefix.split('/', 1)[0]).version
+def get_update_entry(line):
+    """Read output from GoBGP from stdin and return a update entry *dict*"""
+    try:
+        update_list = json.loads(line)
+        for update_entry in update_list:
+            if 'error' in update_entry:
+                return None
+            else:
+                return(update_entry)
+    except Exception as err:
+        C.logging.error("Error in get_update_entry(line):", err)
+        return None
+
+
+def compare_prefixes(new, old):
+    """ignore history, age, and active state, then compare prefix objects"""
+    new['history'] = new['age'] = new['active'] = None
+    old['history'] = old['age'] = old['active'] = None
+    if new == old:
+        return True
+    else:
+        return False
+
+
+def community_32bit_to_string(number):
+    if number is not 0:
+        return f'{int(bin(number)[:-16], 2)}:{int(bin(number)[-16:], 2)}'  # PEP 498
+
+
+def build_json(update_entry):
+    """Given an update entry from GoBGP, set the BGP attribue types as a
+    key/value dict and return"""
+    update_json = {  # set defaults
+        '_id': update_entry['nlri']['prefix'],
+        'route_origin': None,
+        'as_path': [],
+        'nexthop': None,
+        'nexthop_asn': None,
+        'origin_asn': C.DEFAULT_ASN,
+        'med': 0,
+        'local_pref': 0,
+        'atomic_aggregate': None,
+        'aggregator_as': None,
+        'aggregator_address': None,
+        'communities': [],
+        'originator_id': None,
+        'cluster_list': [],
+        'withdrawal': False,
+        'age': 0,
+        'active': True,
+        'history': []
+    }
     for attribute in update_entry['attrs']:
-        if attribute['type'] == 3:
-            nexthop = attribute['nexthop']
-        elif attribute['type'] == 14:
-            nexthop = attribute['nexthop']
-        elif attribute['type'] == 2:
+        if attribute['type'] == C.ORIGIN:
+            update_json['route_origin'] = C.ORIGIN_CODE[attribute['value']]
+        if attribute['type'] == C.AS_PATH:
             try:
-                as_path = attribute['as_paths'][0]['asns']
-            except:
-                as_path = None
+                update_json['as_path'] = attribute['as_paths'][0]['asns']
+                update_json['nexthop_asn'] = update_json['as_path'][0]
+                update_json['origin_asn'] = update_json['as_path'][-1]
+            except Exception:
+                C.logging.debug(f'Error processing as_path: {attribute}')
+                C.logging.debug(f'Error processing as_path: {update_json["_id"]}')
+        if attribute['type'] == C.NEXT_HOP:
+            update_json['nexthop'] = attribute['nexthop']
+        if attribute['type'] == C.MULTI_EXIT_DISC:
             try:
-                next_hop_asn = attribute['as_paths'][0]['asns'][0]
-            except:
-                next_hop_asn = None
+                update_json['med'] = attribute['metric']
+            except Exception:
+                C.logging.debug(f'Error processing med: {attribute}')
+        if attribute['type'] == C.LOCAL_PREF:
             try:
-                origin_as = attribute['as_paths'][0]['asns'][-1]
-            except:
-                origin_as = None
-        elif attribute['type'] == 7:
+                update_json['local_pref'] = attribute['value']
+            except Exception:
+                C.logging.debug(f'Error processing local_pref: {attribute}')
+        if attribute['type'] == C.ATOMIC_AGGREGATE:
+            update_json['atomic_aggregate'] = True
+        if attribute['type'] == C.AGGREGATOR:
+            update_json['aggregator_as'] = attribute['as']
+            update_json['aggregator_address'] = attribute['address']
+        if attribute['type'] == C.COMMUNITY:
             try:
-                origin_as = attribute['as']
-            except:
-                origin_as = None
-        elif attribute['type'] == 4:
-            try:
-                med = attribute['metric']
-            except:
-                med = None
-        elif attribute['type'] == 8:
-            try:
-                communities = []
                 for number in attribute['communities']:
-                    communities.append(str(int(bin(number)[:-16], 2)) + ":" +
-                                       str(int(bin(number)[-16:], 2)))
-            except:
-                communities = []
-        elif attribute['type'] == 5:
-            try:
-                local_pref = attribute['value']
-            except:
-                local_pref = None
-        else:
-            pass
+                    update_json['communities'].append(community_32bit_to_string(number))
+            except Exception:
+                C.logging.debug(f'Error processing communities: {attribute}')
+        if attribute['type'] == C.ORIGINATOR_ID:
+            update_json['originator_id'] = attribute['value']
+        if attribute['type'] == C.CLUSTER_LIST:
+            update_json['cluster_list'] = attribute['value']
+        if attribute['type'] == C.MP_REACH_NLRI:
+            update_json['nexthop'] = attribute['nexthop']
+        if attribute['type'] == C.MP_UNREACH_NLRI:
+            C.logging.debug(f'Found MP_UNREACH_NLRI: {attribute}')
+        if attribute['type'] == C.EXTENDED_COMMUNITIES:
+            C.logging.debug(f'Found EXTENDED_COMMUNITIES: {attribute}')
     if 'withdrawal' in update_entry:
-        withdrawal = update_entry['withdrawal']
-    else:
-        withdrawal = None
+        update_json['withdrawal'] = update_entry['withdrawal']
+        update_json['active'] = False
     if 'age' in update_entry:
-        timestamp = update_entry['age']
-    else:
-        timestamp = None
+        update_json['age'] = datetime.fromtimestamp(update_entry['age']).strftime('%Y-%m-%d %H:%M:%S')
 
-    return {'prefix': prefix,
-            'nexthop': nexthop,
-            'as_path': as_path,
-            'next_hop_asn': next_hop_asn,
-            'origin_as': origin_as,
-            'med': med,
-            'local_pref': local_pref,
-            'withdrawal': withdrawal,
-            'ip_version': ip_version,
-            'communities': communities,
-            'timestamp': timestamp}
+    return update_json
 
 
-def mongo_update(data, db):
-    """Update the mongodb with json BGP update objects."""
-    if data['withdrawal'] is True:
-        result = db.bgp.delete_one({"prefix": data['prefix']})
-        print('Del: %s' % (data['prefix']))
-    else:
-        result = db.bgp.update({"prefix": data['prefix']}, data, upsert=True)
-        if result['nModified'] == 0:
-            print('Add: %s' % (data['prefix']))
-        elif result['nModified'] == 1:
-            print('Mod: %s' % (data['prefix']))
-        else:
-            print('???: %s' % (data['prefix']))
+def update_prefix(prefix_from_gobgp, prefix_from_database):
+    if compare_prefixes(copy(prefix_from_gobgp), copy(prefix_from_database)):
+        prefix_from_gobgp['active'] = True  # flip the active state to true
+    else:  # diff between prefix_from_gobgp and prefix_from_database: update history
+        history_list = prefix_from_database['history']
+        del prefix_from_database['active']  # delete house keeping keys from history objects
+        del prefix_from_database['history']
+        if not history_list:  # no history: create some
+            prefix_from_gobgp['history'].append(prefix_from_database)
+        else:  # existing history: append to history list
+            history_list.insert(0, prefix_from_database)  # insert on top of list, index 0
+            prefix_from_gobgp['history'] = history_list[:C.MAX_PREFIX_HISTORY]  # trim the history list if MAX is set
+    return prefix_from_gobgp
 
 
 def main():
-    """Read GoBGP RIB JSON update lists from stdin and send individual entries
-    to be parsed and fed into Mongo."""
-    db = db_connect()
+    db = db_connect('localhost')
     initialize_database(db)
-    for line in fileinput.input():
-        try:
-            update_list = json.loads(line)
-            for update_entry in update_list:
-                if 'error' in update_entry:
-                    pass
-                else:
-                    mongo_update(build_json_update_entry(update_entry), db)
-        except Exception as err:
-            print(err)
-            pass
+    for line in sys.stdin:
+        prefix_from_gobgp = build_json(get_update_entry(line))
+        prefix_from_database = db.bgp.find_one({'_id': prefix_from_gobgp['_id']})
+        if prefix_from_database:
+            updated_prefix = update_prefix(prefix_from_gobgp, prefix_from_database)
+            db.bgp.update({"_id": prefix_from_database['_id']}, updated_prefix, upsert=True)
+        else:
+            db.bgp.update({"_id": prefix_from_gobgp['_id']}, prefix_from_gobgp, upsert=True)
 
 
 if __name__ == "__main__":
